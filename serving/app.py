dac2c6e436a098423beb9ebc472916b3fd79ff4c
Maneel Reddy Karri <mkarri@dons.usfca.edu>
2023-09-30 16:53:57 -0700

    Adding information

--- serving/app.py
+++ serving/app.py
@@ -1,7 +1,6 @@
 from functools import partial
 from io import BytesIO
 from pathlib import Path
-import wandb
 import torch
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
@@ -10,9 +9,7 @@ from PIL import Image
 from pydantic import BaseModel
 from torch.nn.functional import softmax
 from transformers import ViTImageProcessor
-import os
-logger.info(os.getcwd())
-logger.info(os.listdir('serving'))
+import json
 
 
 class ClassPredictions(BaseModel):
