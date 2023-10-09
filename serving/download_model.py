71047b18e57572999f3195526a5127d59d2907d8
Maneel Reddy Karri <mkarri@dons.usfca.edu>
2023-10-09 00:19:47 -0700

    Back to orig model

--- serving/download_model.py
+++ serving/download_model.py
@@ -4,12 +4,12 @@ import wandb
 
 wandb_team = "maneel"
 wandb_project = "Foodformer"
-wandb_model = "vit:v12"
+wandb_model = "vit:v0"
 wandb_model_path = f"{wandb_team}/{wandb_project}/{wandb_model}"
 
 wandb.init()
 
 current_folder = Path(__file__).parent
 print(f"Folder: {current_folder}")
-path = wandb.use_artifact("maneel/Foodformer/vit:v11").download(root=current_folder)
+path = wandb.use_artifact("maneel/Foodformer/vit:v0").download(root=current_folder)
 print(f"Model downloaded to: {path}")
